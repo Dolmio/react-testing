b984237f71467c3c5665a10ab9f67590e5efb593
Juho Salmio <juho.salmio@gmail.com>
2015-06-03 17:03:40 +0300

    add fetch polyfill

--- app/client.js
+++ app/client.js
@@ -2,7 +2,8 @@
 const React    = require('react'),
       Bacon    = require('baconjs'),
       appState = require('./client/appState'),
-      TodoApp  = require('./client/formApp')
+      TodoApp  = require('./client/formApp'),
+      fetch    = require("whatwg-fetch")
 
 
 const stateStream = appState({
